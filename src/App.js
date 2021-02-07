65fbd9f8509ae93fe4966f6465f815dd827e0a57
I507747 <efecan.yilmaz@sap.com>
2021-02-07 21:04:54 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import AppBar from './components/AppBar';
+import { ArrowRight } from 'react-feather';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <AppBar />
+        <div className="welcome">
+            <div>
+                <p>Welcome! I'm Efe.</p>
+                <span className="main-button">Click to learn more!<ArrowRight className="button-icon"/></span>
+            </div>
+        </div>
     </div>
   );
 }
